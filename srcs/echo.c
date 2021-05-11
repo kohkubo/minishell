2fd6de2e8b6463d0b4059f00d0dc80080cff7ad0
kohkubo <kohkubo@student.42tokyo.jp>
2021-05-11 22:14:09 +0900

    ft_echo関数の切り分け方を改善

--- srcs/echo.c
+++ srcs/echo.c
@@ -1,19 +1,11 @@
 #include "../includes/shell.h"
 #include "../libft/libft/libft.h"
 
-static void	print_n(int flg)
+static int	ft_echo_do(va_list ap, int n)
 {
-	if (flg)
-		printf("\n");
-}
-
-int	ft_echo(int n, ...)
-{
-	va_list	ap;
 	int		op_flg;
 	char	*arg;
 
-	va_start(ap, n);
 	op_flg = 0;
 	if (n > 0)
 	{
@@ -31,7 +23,18 @@ int	ft_echo(int n, ...)
 		while (--n > 0)
 			printf(" %s", va_arg(ap, char *));
 	}
-	print_n(!op_flg);
+	if (!op_flg)
+		printf("\n");
+	return (0);
+}
+
+int	ft_echo(int n, ...)
+{
+	va_list	ap;
+	int		ret;
+
+	va_start(ap, n);
+	ret = ft_echo_do(ap, n);
 	va_end(ap);
 	return (0);
 }
