a958d1b21912b98d2807cb35d03247af48157906
kohkubo <kohkubo@student.42tokyo.jp>
2021-05-11 22:14:23 +0900

    norm対応

--- tests/unit-test/function-ft_echo_test/test.c
+++ tests/unit-test/function-ft_echo_test/test.c
@@ -8,7 +8,7 @@ int	main(void)
 	ft_echo(3, "-n", "aiueo", "ttttt");
 	ft_echo(1, "-n");
 	ft_echo(1, "");
-	ft_echo(2, "-n","");
+	ft_echo(2, "-n", "");
 	ft_echo(NULL);
 	ft_echo(1, NULL);
 	ft_echo(2, "test", NULL);
