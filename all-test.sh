6bfaf46ad81af0976b51e25a7ad53423d0712417
kohkubo <kohkubo@student.42tokyo.jp>
2021-05-11 12:01:32 +0900

    Revert "Revert "feat: 入力をオウム返しテストケース #12""
    This reverts commit faf10d05608bfca3b1bda1624dc5388ba8e63dc0.

--- all-test.sh
+++ all-test.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 set -e
 make
-for path in `find ./tests -type f -name test.sh`;
-do echo $path; bash $path;
+
+for path in $(find ./tests -type f -name test.sh);
+	do echo "$path"; bash "$path";
 done
