6f9c6654bc37118494b830aad282c507cf0a3455
kohkubo <kohkubo@student.42tokyo.jp>
2021-05-12 01:37:04 +0900

    ft_echoのNULLチェックを強化、 テストケースも追加

--- tests/unit-test/function-ft_echo_test/test.c
+++ tests/unit-test/function-ft_echo_test/test.c
@@ -1,18 +1,50 @@
 #include <stdio.h>
+#include <libc.h>
 #include "./../../../includes/shell.h"
 
 int	main(void)
 {
-	ft_echo(1, "aiueo");
-	ft_echo(2, "aiueo", "ttttt");
-	ft_echo(3, "-n", "aiueo", "ttttt");
-	ft_echo(1, "-n");
-	ft_echo(1, "");
-	ft_echo(2, "-n", "");
-	ft_echo(NULL);
-	ft_echo(1, NULL);
-	ft_echo(2, "test", NULL);
-	ft_echo(2, NULL, NULL);
-	ft_echo(0);
+	int backup = dup(STDOUT_FILENO);
+	int fd = open("./output", O_RDWR);
+	int i = dup2(fd, STDOUT_FILENO);
+
+	if (ft_echo(1, "aiueo") != 0)
+		exit(1);
+	fflush(stdout);
+	if (ft_echo(2, "aiueo", "test") != 0)
+		exit(1);
+	fflush(stdout);
+	if (ft_echo(3, "-n", "aiueo", "test") != 0)
+		exit(1);
+	fflush(stdout);
+	if (ft_echo(1, "-n") != 0)
+		exit(1);
+	fflush(stdout);
+	if (ft_echo(1, "") != 0)
+		exit(1);
+	fflush(stdout);
+	if (ft_echo(2, "-n", "") != 0)
+		exit(1);
+	fflush(stdout);
+	if (ft_echo(0, NULL) != 0)
+		exit(1);
+	fflush(stdout);
+
+	// エラーケース
+	if (ft_echo(1, NULL) != 1)
+		exit(1);
+	fflush(stdout);
+	if (ft_echo(2, "test", NULL) != 1)
+		exit(1);
+	fflush(stdout);
+	if (ft_echo(2, NULL, NULL) != 1)
+		exit(1);
+	fflush(stdout);
+	if (ft_echo(0) != 0)
+		exit(1);
+	fflush(stdout);
+	dup2(backup, STDOUT_FILENO);
+	close(backup);
+
 	return (0);
 }
