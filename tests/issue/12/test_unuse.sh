73b2f9518034da05b623fa865da903960f98d146
kohkubo <kohkubo@student.42tokyo.jp>
2021-05-12 01:43:08 +0900

    テストケースを編集 終了ステータスが反映されるように変更

--- tests/issue/12/test_unuse.sh
+++ tests/issue/12/test_unuse.sh
@@ -3,49 +3,49 @@
 set -e
 
 echo "=============test12================"
-cp minishell "$(dirname "$0")"
+cp ./minishell "$(dirname "$0")"
 cd "$(dirname "$0")" || exit
 
 # 通常出力
-echo "test" | minishell > output
+echo "test" | ./minishell > output
 echo "test" > expect
 
 # スペース区切り
-echo "aaa bbb ccc" | minishell >> output
+echo "aaa bbb ccc" | ./minishell >> output
 echo -e 'aaa\nbbb\nccc' >> expect
 
-echo "echo" | minishell >> output
+echo "echo" | ./minishell >> output
 echo "echo is not implemented yet" >> expect
 
-echo "cd" | minishell >> output
+echo "cd" | ./minishell >> output
 echo "cd is not implemented yet" >> expect
 
-echo "pwd" | minishell >> output
+echo "pwd" | ./minishell >> output
 echo "pwd is not implemented yet" >> expect
 
-echo "export" | minishell >> output
+echo "export" | ./minishell >> output
 echo "export is not implemented yet" >> expect
 
-echo "unset" | minishell >> output
+echo "unset" | ./minishell >> output
 echo "unset is not implemented yet" >> expect
 
-echo "env" | minishell >> output
+echo "env" | ./minishell >> output
 echo "env is not implemented yet" >> expect
 
-echo "exit" | minishell >> output
+echo "exit" | ./minishell >> output
 echo "exit is not implemented yet" >> expect
 
-echo "cd cd cd" | minishell >> output
+echo "cd cd cd" | ./minishell >> output
 echo "cd is not implemented yet" >> expect
 
-echo " pwd echo" | minishell >> output
+echo " pwd echo" | ./minishell >> output
 echo "pwd is not implemented yet" >> expect
 
-echo "aaa echo" | minishell >> output
+echo "aaa echo" | ./minishell >> output
 echo -e "aaa\necho" >> expect
 
-echo "" | minishell
-echo "    " | minishell
+echo "" | ./minishell
+echo "    " | ./minishell
 
 diff output expect
 RES=$?
