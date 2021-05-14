0438246b6b56396e18bc10ff5c5e0b2e49f96c83
kohkubo <kohkubo@student.42tokyo.jp>
2021-05-14 14:59:22 +0900

    feat: #22 make headerの作成, header.shの追加

--- libft/libft/libft.h
+++ libft/libft/libft.h
@@ -3,13 +3,12 @@
 # include "../libex/libex.h"
 # include <unistd.h>
 # include <stdlib.h>
-// Prototypes
-size_t	ft_strlen(const char *s);
-void	ft_putstr_fd(char *s, int fd);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *s, int c);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putstr_fd(char *s, int fd);
 char	**ft_split(char const *s, char c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif
