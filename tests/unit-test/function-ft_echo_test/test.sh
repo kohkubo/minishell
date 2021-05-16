6c895427b082d9ad803987edd078dad53d3cd3cc
kohkubo <kohkubo@student.42tokyo.jp>
2021-05-16 17:47:13 +0900

    test: ft_echoのunit testを追加

--- tests/unit-test/function-ft_echo_test/test.sh
+++ tests/unit-test/function-ft_echo_test/test.sh
@@ -13,7 +13,6 @@ test_res_print() {
 	fi
 }
 
-echo "=============ft_echo================"
 cd "$(dirname "$0")" || exit
 
 gcc test.c $(find $REPO_ROOT/srcs -type f -name echo.c) $REPO_ROOT/libft/libft/libft.a $REPO_ROOT/libft/libex/libex.a
