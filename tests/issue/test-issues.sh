6429a53e20c7b3049520f471ee26c0ee4cc4f34d
ywake <ywake@student.42tokyo.jp>
2021-05-17 19:52:11 +0900

    Add: テスト後にOK/KO表記を追加

--- tests/issue/test-issues.sh
+++ tests/issue/test-issues.sh
@@ -7,5 +7,12 @@ make
 export REPO_ROOT=$PWD
 
 for path in $(find ./tests/issue -type f -name test.sh);
-	do echo "$path"; bash "$path";
+do
+	echo "===== $path ====="
+	bash "$path"
+	if [ $? -eq 0 ]; then
+		printf "\e[32m%s\n\e[m" "OK!"
+	else
+		printf "\e[31m%s\n\e[m" "KO!"
+	fi
 done
