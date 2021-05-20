60f7c43b5c503d4f5f433e17ce143413a3b313f7
kohkubo <kohkubo@student.42tokyo.jp>
2021-05-20 15:31:39 +0900

    feat(ft_strerror.c errlst.c libex.h Makefile): ft_strerror.cを追加

--- libft/libex/libex.h
+++ libft/libex/libex.h
@@ -2,12 +2,5 @@
 # define LIBEX_H
 # include "../libft/libft.h"
 # include <stdlib.h>
-int		contain(char **array, char *word);
-void	free_set(void **dst, void *src);
-char	**free_string_array(char **buf);
-int		ft_isspace(int c);
-char	*ft_strndup(const char *s1, size_t n);
-void	*ft_xmalloc(size_t size);
-int		get_next_line(int fd, char **line);
-int		min(int a, int b);
+char			*ft_strerror(int e);
 #endif
