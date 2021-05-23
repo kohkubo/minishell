e9b74748c74255a32adb5bc1924da2d50e3dcac6
kohkubo <kohkubo@student.42tokyo.jp>
2021-05-23 12:25:15 +0900

    feat(ft_strcmp.c libex.h Makefile): ft_strcmpを追加

--- libft/libex/libex.h
+++ libft/libex/libex.h
@@ -3,6 +3,7 @@
 # include "../libft/libft.h"
 # include <stdlib.h>
 void			ft_fatal(const char *emsg);
+int				ft_strcmp(const char *s1, const char *s2);
 char			*ft_strerror(int e);
 void			*ft_xmalloc(size_t size);
 #endif
