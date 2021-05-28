4f72abf93e81f2e0a8cf5f0181221068c96ddbd2
ywake <ywake@student.42tokyo.jp>
2021-05-28 17:00:45 +0900

    fix: CIでエラーが拾えてなかったのを修正 コンパイルワーニングの除去。 all-testでKOを拾えてなかったのを修正。 array_cmpの修正。

--- all-test.sh
+++ all-test.sh
@@ -7,6 +7,8 @@ make leak
 export REPO_ROOT="$PWD"
 export SHARED_LIB="$PWD/tests/sharedlib.c"
 
+FLG=0
+
 for path in $(find $DIR -type f -name test.sh);
 do
 	echo "===== $path ====="
@@ -15,5 +17,8 @@ do
 		printf "\e[32m%s\n\e[m" "OK!"
 	else
 		printf "\e[31m%s\n\e[m" "KO!"
+		FLG=1
 	fi
 done
+
+exit $FLG
