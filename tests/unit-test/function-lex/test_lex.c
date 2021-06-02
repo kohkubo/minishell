0115fdd37ebc8bc907adaa92dd85fd5da40a85dc
kohkubo <kohkubo@student.42tokyo.jp>
2021-06-02 15:33:45 +0900

    test(tests/unit-test/function-lex): テストケースの追加、出力結果の機械判定の追加

--- tests/unit-test/function-lex/test_lex.c
+++ tests/unit-test/function-lex/test_lex.c
@@ -14,16 +14,6 @@ void	display_lexer(void *content)
 	printf("%s : %d\n", tok->data, tok->type);
 }
 
-void	lexer_free(void *content)
-{
-	t_tok	*tok;
-
-	tok = (t_tok *)content;
-	free(tok->data);
-	free(tok);
-}
-
-
 void test(char *s)
 {
 	t_lexer	*lexer;
@@ -31,30 +21,34 @@ void test(char *s)
 	printf(" === test === \n");
 	printf("s : %s\n", s);
 	lexer = minishell_lexer(s);
+	if (lexer == NULL)
+		return ;
 	ft_lstiter(lexer->listtok, display_lexer);
-	ft_lstclear(&lexer->listtok, lexer_free);
-	free(lexer);
+	lexer_free(&lexer);
 }
 
 int main(void)
 {
-	// test("echo test test");
-	test("      echo     test      test    ");
-	// test("   echo test test");
-	// test("echo test test  ");
-	// test("echo test ;test");
-	// test("echo test |test");
-	// test("echo test >test");
-	// test("echo test < test");
-	// test("echo test >> test");
-	// test("echo test << test");
-	// test("'echo test << test'");
-	// test("echo 'echo test << test'");
-	// test("'echo test << test' eeee");
-	// test("aa 'echo test << test' eeee");
-	// test("'echo test << test''sssssssssssss'");
-	// test("'echo test << test'\"sssssssssssss\"");
-	// test("\"echo ssssss\"");
-	// test("echo test ;\n\taaaaa");
-	// test("echo test ;;");
+	test("echo test aiueo");
+	test("      echo     test      aiueo    ");
+	test("   echo test aiueo");
+	test("echo test aiueo  ");
+	test("echo test ;aiueo");
+	test("echo test |aiueo");
+	test("echo test >aiueo");
+	test("echo test < aiueo");
+	test("echo test >> aiueo");
+	test("echo test << aiueo");
+	test("'echo test << aiueo'");
+	test("echo 'echo test << aiueo'");
+	test("'echo test << test' eeee");
+	test("aa 'echo test << test' eeee");
+	test("'echo test << test''sssssssssssss'");
+	test("'echo test << test'\"sssssssssssss\"");
+	test("\"echo ssssss\"");
+	test("echo test ;\n\taaaaa");
+	test("echo test ;;");
+	test(";;");
+	test(" ");
+	test("           ");
 }
