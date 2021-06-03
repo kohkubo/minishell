cce18a768386a094af7d68fd292c9699b084207d
ywake <ywake@student.42tokyo.jp>
2021-06-03 16:24:51 +0900

    Impl: <command> のパース実装

---
+++ srcs/parse/command.c
@@ -0,0 +1,67 @@
+#include "parse.h"
+
+t_astree	*cmd_redirect(t_list **toks, t_token_type t, t_node_type n);
+t_astree	*cmd1(t_list **toks); // <simple command>
+
+/**
+ * <command>	::= <simple command> '<' <filename>
+ *				  | <simple command> '>' <filename>
+ *				  | <simple command> '<<' <filename>
+ *				  | <simple command> '>>' <filename>
+ *				  | <simple command>
+ */
+t_astree	*cmd(t_list **toks)
+{
+	t_list		*save;
+	t_astree	*result;
+
+	save = *toks;
+	result = cmd_redirect(toks, CHAR_LESSER, NODE_REDIRECT_IN);
+	if (result != NULL)
+		return (result);
+	*toks = save;
+	result = cmd_redirect(toks, CHAR_GREATER, NODE_REDIRECT_OUT);
+	if (result != NULL)
+		return (result);
+	*toks = save;
+	result = cmd_redirect(toks, CHAR_LESSER2, NODE_REDIRECT_IN2);
+	if (result != NULL)
+		return (result);
+	*toks = save;
+	result = cmd_redirect(toks, CHAR_GREATER2, NODE_REDIRECT_OUT2);
+	if (result != NULL)
+		return (result);
+	*toks = save;
+	return (cmd1(toks));
+}
+
+/**
+ * <command> ::= <simple command> t <filename>
+ */
+t_astree	*cmd_redirect(t_list **toks, t_token_type t, t_node_type n)
+{
+	t_astree	*simplecmd_node;
+	t_astree	*result;
+	char		*filename;
+
+	simplecmd_node = simplecmd(toks);
+	if (simplecmd_node == NULL)
+		return (NULL);
+	if (!is_tokentype(t, toks))
+		return (astree_delete_node(simplecmd_node));
+	filename = ft_strdup(((t_tok *)(*toks)->content)->data);
+	if (!is_tokentype(TOKEN, toks))
+	{
+		free(filename);
+		return (astree_delete_node(simplecmd_node));
+	}
+	return (astree_create_node(n | NODE_DATA, filename, NULL, simplecmd_node));
+}
+
+/**
+ * <command> ::= <simple command>
+ */
+t_astree	*cmd1(t_list **toks)
+{
+	return (simplecmd(toks));
+}
