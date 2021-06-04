8e475125261a87f2a4e22a9657c939dc6c58ac26
kohkubo <kohkubo@student.42tokyo.jp>
2021-06-04 12:59:47 +0900

    refactor(lex.h): 構造体のインデントを揃える

--- includes/lex.h
+++ includes/lex.h
@@ -6,30 +6,30 @@
 
 typedef enum e_token_type
 {
-	CHAR_GENERAL = -1,
-	CHAR_PIPE = '|',
-	CHAR_AMPERSAND = '&',
-	CHAR_QOUTE = '\'',
-	CHAR_DQUOTE = '\"',
-	CHAR_SEMICOLON = ';',
-	CHAR_SEMICOLON2 = ';' + PAD,
-	CHAR_WHITESPACE = ' ',
-	CHAR_ESCAPESEQUENCE = '\\',
-	CHAR_TAB = '\t',
-	CHAR_NEWLINE = '\n',
-	CHAR_GREATER = '>',
-	CHAR_GREATER2 = '>' + PAD,
-	CHAR_LESSER = '<',
-	CHAR_LESSER2 = '<' + PAD,
-	CHAR_NULL = 0,
-	TOKEN	= -1,
+	CHAR_GENERAL		= -1,
+	CHAR_PIPE			= '|',
+	CHAR_AMPERSAND		= '&',
+	CHAR_QOUTE			= '\'',
+	CHAR_DQUOTE			= '\"',
+	CHAR_SEMICOLON		= ';',
+	CHAR_SEMICOLON2		= ';' + PAD,
+	CHAR_WHITESPACE		= ' ',
+	CHAR_ESCAPESEQUENCE	= '\\',
+	CHAR_TAB			= '\t',
+	CHAR_NEWLINE		= '\n',
+	CHAR_GREATER		= '>',
+	CHAR_GREATER2		= '>' + PAD,
+	CHAR_LESSER			= '<',
+	CHAR_LESSER2		= '<' + PAD,
+	CHAR_NULL			= 0,
+	TOKEN				= -1,
 }	t_token_type;
 
 typedef enum e_state_type{
-	STATE_IN_DQUOTE = '"',
-	STATE_IN_QUOTE = '\'',
-	STATE_GENERAL = -1,
-	STATE_ERROR = -2,
+	STATE_IN_DQUOTE	= '"',
+	STATE_IN_QUOTE	= '\'',
+	STATE_GENERAL	= -1,
+	STATE_ERROR		= -2,
 }	t_state_type;
 
 /*
