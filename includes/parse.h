c6ff088035b105ddb7960409dfad0035a6fa4513
ywake <ywake@student.42tokyo.jp>
2021-06-10 18:36:15 +0900

    add: #70 対応

--- includes/parse.h
+++ includes/parse.h
@@ -8,6 +8,8 @@ bool		is_tokentype(t_token_type type, t_list **current, char **buf_ptr);
 t_astree	*cmdline(t_list **toks);
 t_astree	*job(t_list **toks);
 t_astree	*cmd(t_list **toks);
+t_astree	*redirlist(t_list **toks);
+t_astree	*redirection(t_list **toks);
 t_astree	*simplecmd(t_list **toks);
 t_astree	*tokenlist(t_list **toks);
 
