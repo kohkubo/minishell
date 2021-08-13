2863cf288a1c2b5c38faf890caa33cc21fea9c92
kohkubo <kohkubo@student.42tokyo.jp>
2021-08-13 22:01:06 +0900

    feat(libex): add func spacelen.c

--- libft/libex/libex.h
+++ libft/libex/libex.h
@@ -32,5 +32,6 @@ int		ft_isspace(int c);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_xstrdup(const char *s1);
 bool	is_space_string(char *s);
+size_t	spacelen(char *s);
 
 #endif
---
+++ libft/libex/spacelen.c
@@ -0,0 +1,11 @@
+#include "libex.h"
+
+size_t	spacelen(char *s)
+{
+	size_t	i;
+
+	i = 0;	
+	while (ft_isspace(s[i]))
+		i++;
+	return (i);
+}
