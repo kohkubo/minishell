5888fad97eec800b6eb8c6faeb81f0a6e8c16906
kohkubo <kohkubo@student.42tokyo.jp>
2021-08-13 13:29:13 +0900

    feat: lex_analyze.hをlex.hに統合

--- includes/lex.h
+++ includes/lex.h
@@ -62,9 +62,18 @@ typedef struct s_lexer
 	t_list	*listtok;
 }	t_lexer;
 
-void	lexer_free(t_lexer **lexer);
-void	tok_free(void *content);
-t_tok	*tok_init(char *s);
-t_lexer	*lexer_init(void);
+char			*shell_prompt(t_state_type state);
+t_token_type	token_type(char c);
+void			token_end(t_lexer *lexer, t_tok **tok, size_t n);
+void			token_end_and_create(t_lexer *lexer, t_tok **tok, char *s, size_t *n);
+void			token_store_and_create(t_lexer *l, t_tok **t, char *s, t_token_type typ);
+void			token_store2_and_create(t_lexer *l, t_tok **t, char **s);
+char			*heredoc_readline(char *heredoc, char *tok);
+char			*generate_heredoc(char *s);
+t_tok			*tok_init(char *s);
+t_lexer			*lexer_init(void);
+void			tok_free(void *content);
+void			lexer_free(t_lexer **lexer);
+t_state_type	heredoc(t_lexer **l, t_tok **tok, char **s, size_t *i);
 
 #endif
