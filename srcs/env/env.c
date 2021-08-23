96be51ea8b2ca96affd3d00d7e1bc24314f42afc
kohkubo <kohkubo@student.42tokyo.jp>
2021-08-23 16:56:22 +0900

    refactor(store_shellenv): グローバル変数に依存しないように引数を変更

--- srcs/env/env.c
+++ srcs/env/env.c
@@ -6,13 +6,13 @@
 /*   By: kohkubo <kohkubo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 20:23:32 by kohkubo           #+#    #+#             */
-/*   Updated: 2021/08/19 12:19:36 by kohkubo          ###   ########.fr       */
+/*   Updated: 2021/08/23 16:54:25 by kohkubo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-void	store_shellenv(char **arr)
+void	store_shellenv(char **arr, t_hash_table *table)
 {
 	char		*tmp;
 	size_t		i;
@@ -24,7 +24,7 @@ void	store_shellenv(char **arr)
 		{
 			tmp = ft_strdup(arr[i]);
 			tmp[ft_strchr(tmp, '=') - tmp] = '\0';
-			hash_setstr(g_shell.hash, tmp, tmp + ft_strlen(tmp) + 1);
+			hash_setstr(table, tmp, tmp + ft_strlen(tmp) + 1);
 			free_set((void **)&tmp, NULL);
 		}
 		i++;
@@ -35,11 +35,11 @@ static void	env_init(void)
 {
 	extern char	**environ;
 
-	store_shellenv(environ);
+	store_shellenv(environ, g_shell.env);
 }
 
 void	minishell_init(void)
 {
-	g_shell.hash = hash_create_table(10);
+	g_shell.env = hash_create_table(10);
 	env_init();
 }
