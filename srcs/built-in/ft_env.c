cdab3c8559694ce419e1f59ab1bfab7611f87063
kohkubo <kohkubo@student.42tokyo.jp>
2021-08-23 16:57:59 +0900

    fix(ft_env): bashのenvの挙動に合わせ、引数にKey=valueの値が来たときに環境変数に影響を与えないように変更

--- srcs/built-in/ft_env.c
+++ srcs/built-in/ft_env.c
@@ -5,10 +5,17 @@ extern t_shell	g_shell;
 int	ft_env(char **args)
 {
 	char	**env;
+	char	**tmp;
+	t_hash_table *env_hash;
 
-	store_shellenv(args);
-	env = hash_getall(g_shell.hash);
+	tmp = hash_getall(g_shell.env);
+	env_hash = hash_create_table(1024);
+	store_shellenv(tmp, env_hash);
+	store_shellenv(args, env_hash);
+	env = hash_getall(env_hash);
 	print_array(env);
 	free_string_array(env);
+	free_string_array(tmp);
+	hash_clear_table(&env_hash);
 	return (0);
 }
