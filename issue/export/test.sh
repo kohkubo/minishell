2622e72cf572398fe93c9dde69805fa60c56250d
kohkubo <kohkubo@student.42tokyo.jp>
2021-08-24 21:22:58 +0900

    feat(export): exportのテストケース

---
+++ issue/export/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cp ./minishell "$(dirname "$0")"
+
+cd "$(dirname "$0")" || exit
+
+echo $PWD
+
+
+for file in $(ls *.txt); do
+	./minishell < $file >> output 2>&1
+	bash < $file >> expect 2>&1
+	grep -v -e "SHLVL" -v -e "_=" -v -e "MAKEOVERRIDES" expect | sort --output=expect
+	grep -v -e "SHLVL" -v -e "_=" -v -e "MAKEOVERRIDES" output | sort --output=output
+	diff expect output
+	if [ $? -ne 0 ]; then
+		echo "FAIL: $file"
+		RES=1
+	fi
+	rm -rf output expect
+done
+
+rm -f a.out leaksout minishell
+rm -rf a.out.dSYM
+
+exit $RES
