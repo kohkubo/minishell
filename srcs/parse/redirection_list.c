e258e3e7869d2e17be6b70ab53bf5c6452d769ce
ywake <ywake@student.42tokyo.jp>
2021-08-24 20:08:10 +0900

    fix: redirection1つの対応ができなくなっていたのを修正

--- srcs/parse/redirection_list.c
+++ srcs/parse/redirection_list.c
@@ -1,13 +1,23 @@
 #include "parse.h"
 
-t_astree	*redirlist1(t_list **toks);
+t_astree	*redirlist1(t_list **toks); // <redirection> <redirection list>
+t_astree	*redirlist2(t_list **toks); // <redirection>
 
 /**
  * <redirection list> ::= <redirection> <redirection list>
+ * 						| <redirection>
  */
 t_astree	*redirlist(t_list **toks)
 {
-	return (redirlist1(toks));
+	t_list		*save;
+	t_astree	*node;
+
+	save = *toks;
+	node = redirlist1(toks);
+	if (node != NULL)
+		return (node);
+	*toks = save;
+	return (redirlist2(toks));
 }
 
 /**
@@ -26,4 +36,18 @@ t_astree	*redirlist1(t_list **toks)
 		return (astree_delete_node(redirection_node));
 	return (astree_create_node(NODE_REDIRECT_LIST, NULL,
 			redirection_node, redirlist_node));
+}
+
+/**
+ * <redirection list> ::= <redirection>
+ */
+t_astree	*redirlist2(t_list **toks)
+{
+	t_astree	*redirection_node;
+
+	redirection_node = redirection(toks);
+	if (redirection_node == NULL)
+		return (NULL);
+	return (astree_create_node(NODE_REDIRECT_LIST, NULL,
+			redirection_node, NULL));
 }
