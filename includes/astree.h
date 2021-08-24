2aac52e356bc21128e2a25a050e1347ba02001b7
ywake <ywake@student.42tokyo.jp>
2021-08-24 20:54:47 +0900

    fix: for norm astree.h

--- includes/astree.h
+++ includes/astree.h
@@ -3,16 +3,16 @@
 
 typedef enum e_node_type
 {
-	NODE_PIPE 			= (1 << 0),
-	NODE_BCKGRND 		= (1 << 1),
-	NODE_SEQ 			= (1 << 2),
+	NODE_PIPE			= (1 << 0),
+	NODE_BCKGRND		= (1 << 1),
+	NODE_SEQ			= (1 << 2),
 	NODE_REDIRECT_IN	= (1 << 3),
 	NODE_REDIRECT_OUT	= (1 << 4),
 	NODE_REDIRECT_IN2	= (1 << 5),
 	NODE_REDIRECT_OUT2	= (1 << 6),
 	NODE_CMDPATH		= (1 << 7),
 	NODE_ARGUMENT		= (1 << 8),
-	NODE_DATA 			= (1 << 9),
+	NODE_DATA			= (1 << 9),
 	NODE_REDIRECT_LIST	= (1 << 10),
 	NODE_REDIRECTION	= (1 << 11),
 }	t_node_type;
