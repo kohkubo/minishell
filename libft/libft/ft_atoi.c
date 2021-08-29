f06f9ff5f33b4065a0884b8e3e12155732d4e404
kohkubo <kohkubo@student.42tokyo.jp>
2021-08-29 13:34:40 +0900

    feat(libft): ft_atoiでint型をオーバーフローしたときの処理を追加

--- libft/libft/ft_atoi.c
+++ libft/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: kohkubo <kohkubo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 16:02:27 by kohkubo           #+#    #+#             */
-/*   Updated: 2021/08/25 11:30:39 by kohkubo          ###   ########.fr       */
+/*   Updated: 2021/08/29 13:32:00 by kohkubo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,20 @@ static int	ft_isspace(int c)
 	return (('\t' <= c && c <= '\r') || c == ' ');
 }
 
+static int	ft_atoi_overflow(int neg)
+{
+	errno = EINVAL;
+	if (neg == 1)
+		return ((int)LONG_MAX);
+	else
+		return ((int)LONG_MIN);
+}
+
 int	ft_atoi(const char *s)
 {
-	uint64_t	n;
-	int			neg;
-	uint64_t	tmp;
+	unsigned long	n;
+	int				neg;
+	unsigned long	tmp;
 
 	n = 0;
 	neg = 1;
@@ -36,13 +45,9 @@ int	ft_atoi(const char *s)
 		tmp = n;
 		n = n * 10 + *s++ - '0';
 		if (tmp >> 60 || n >> 63)
-		{
-			errno = EINVAL;
-			if (neg == 1)
-				return ((int)LONG_MAX);
-			else
-				return ((int)LONG_MIN);
-		}
+			return (ft_atoi_overflow(neg));
 	}
+	if (n > INT_MAX || (n == (unsigned long)INT_MAX + 1 && neg == -1))
+		errno = EINVAL;
 	return ((int)(n * neg));
 }
