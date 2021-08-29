2fffd7e1912fbb0e53e7a5bcd2f9a22430aa9936
kohkubo <kohkubo@student.42tokyo.jp>
2021-08-29 13:36:07 +0900

    feat(ft_exit.c/ft_exit()): ft_exitの実装

--- srcs/built-in/ft_exit.c
+++ srcs/built-in/ft_exit.c
@@ -1,8 +1,16 @@
 #include "shell.h"
 
-int	ft_exit(char **arg)
+int	ft_exit(char **args)
 {
-	(void)arg;
-	ft_putendl_fd("exit is not implemented yet", 1);
+	if (args[0] == NULL)
+		exit(0);
+	if (arraylen(args) != 1)
+	{
+		ft_putstr_fd("exit: too many arguments\n", 2);
+		return (1);
+	}
+	if (!is_num_string(args[0]))
+		exit(0);
+	exit((int)ft_atol(args[0]));
 	return (0);
 }
