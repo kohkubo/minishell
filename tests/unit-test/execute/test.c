1668a800d711a8b19a2f477d0a6cf5151083636a
ywake <ywake@student.42tokyo.jp>
2021-09-06 17:21:51 +0900

    Change: Separated error case tests & Added error message / status code comparisons.

--- tests/unit-test/execute/test.c
+++ tests/unit-test/execute/test.c
@@ -6,11 +6,13 @@ int main(int argc, char *argv[])
 {
 	t_lexer		*lex;
 	t_astree	*tree;
+	int			status;
 
 	minishell_init();
 	minishell_lexer(argv[1], &lex);
 	parse_v2(lex, &tree);
-	minishell_execute(tree);
+	status = minishell_execute(tree);
 	lexer_free(&lex);
 	tree = astree_delete_node(tree);
+	return (status);
 }
--- tests/unit-test/execute/test.sh
+++ tests/unit-test/execute/test.sh
@@ -18,9 +18,6 @@ tests=(
 	"$DIR/tester.py"
 	"$DIR/tester.py arg1 arg2"
 	# "ls | cat | cat"
-	"no_exist_command"
-	"./no_exist_file"
-	"$DIR/no_perm.py"
 )
 
 for i in ${!tests[@]};
@@ -36,6 +33,34 @@ do
 	else
 		EXIT_CODE=1
 		diff -y "$DIR/out" "$DIR/expect"
+		printf "\e[31m%s\n\e[m" ">>  KO!"
+	fi
+done
+
+echo "--- error case ---"
+
+errors=(
+	"no_exist_command"
+	"./no_exist_file"
+	"$DIR/no_perm.py"
+)
+
+for i in ${!errors[@]};
+do
+	echo "./a.out ${errors[$i]}"
+	echo "${errors[$i]}" | bash &> $DIR/expect
+	BASH_EXIT_CODE=$?
+	"$DIR/a.out" "${errors[$i]}" &> $DIR/out
+	AOUT_EXIT_CODE=$?
+
+	diff <(cat "$DIR/out" | sed 's/minishell:/bash: line 1:/') "$DIR/expect" > /dev/null
+	# if [ $? -eq 0 ] && [ $AOUT_EXIT_CODE -eq $BASH_EXIT_CODE ]; then
+	if [ $? -eq 0 ] && [ $AOUT_EXIT_CODE -ne 0 ]; then
+		printf "\e[32m%s\n\e[m" ">>  OK!"
+	else
+		EXIT_CODE=1
+		echo "AOUT_EXIT_CODE: $AOUT_EXIT_CODE, BASH_EXIT_CODE: $BASH_EXIT_CODE"
+		diff <(cat "$DIR/out" | sed 's/minishell:/bash: line 1:/') "$DIR/expect" -y
 		printf "\e[31m%s\n\e[m" ">>  KO!"
 	fi
 done
