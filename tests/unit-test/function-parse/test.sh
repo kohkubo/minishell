f187acf81bac10e0af7a3447d9f7c2788acabcae
ywake <ywake@student.42tokyo.jp>
2021-09-08 17:47:06 +0900

    Add: パースエラー時の出力をチェックする

--- tests/unit-test/function-parse/test.sh
+++ tests/unit-test/function-parse/test.sh
@@ -34,17 +34,20 @@ $(find $REPO_ROOT/srcs/lex/ -type f -name "*.c") \
 $(find $REPO_ROOT/srcs/parse/ -type f -name "*.c" -not -name 'parse.c') \
 $SHARED_LIB $LIBS -lft -lex -lreadline || exit 1
 
-$DIR/a.out
+$DIR/a.out 2> $DIR/out
 AOUT=$?
 
-if [ $AOUT -ne 0 ]; then
+diff $DIR/out $DIR/error_case_expect > /dev/null
+
+if [ $? -ne 0 ] && [ $AOUT -ne 0 ]; then
 	EXIT_CODE=1
+	diff -y $DIR/out $DIR/error_case_expect
 	printf "\e[31m%s\n\e[m" ">>  KO!"
 else
 	printf "\e[32m%s\n\e[m" ">>  OK!"
 fi
 
-rm -f $DIR/a.out leaksout
+rm -f $DIR/a.out leaksout $DIR/out
 rm -rf $DIR/a.out.dSYM
 
 exit $EXIT_CODE
