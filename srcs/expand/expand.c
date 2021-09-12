08ef594a2cf7902ea9963ed829b78b16417ca257
kohkubo <kohkubo@student.42tokyo.jp>
2021-09-12 18:57:22 +0900

    feat(expand.c/minishell_expand(),-expand.c/separate_to_list()): シングルクォート、ダブルクォートの展開、アルゴリズムの変更

--- srcs/expand/expand.c
+++ srcs/expand/expand.c
@@ -2,40 +2,75 @@
 
 static void	separate_to_list(char *arg, t_list **list)
 {
-	char	*start;
-	char	*end;
+	char	*buf;
+	int		i;
 
+	buf = ft_xmalloc(sizeof(char) * (ft_strlen(arg) + 1));
 	while (*arg != '\0')
 	{
-		start = ft_strchr(arg, CHAR_DOL);
-		if (start == NULL)
+		if (*arg == CHAR_DOL || *arg == CHAR_QUOTE
+			|| *arg == CHAR_DQUOTE || *arg == CHAR_SPACE)
 		{
-			ft_lstadd_back(list, ft_lstnew(ft_xstrdup(arg)));
-			break ;
+			ft_lstadd_back(list, ft_lstnew(ft_xsubstr(arg, 0, 1)));
+			arg++;
 		}
-		if (start != arg)
-			ft_lstadd_back(list, ft_xlstnew(ft_xsubstr(arg, 0, start - arg)));
-		end = start + 1;
-		while (ft_strchr("$", *end) == NULL)
-			end++;
-		ft_lstadd_back(list, ft_xlstnew(ft_xsubstr(start, 0, end - start)));
-		arg = end;
+		else
+		{
+			i = 0;
+			while (*arg != '\0'
+				&& *arg != CHAR_DOL && *arg != CHAR_QUOTE
+				&& *arg != CHAR_DQUOTE && *arg != CHAR_SPACE)
+				buf[i++] = *arg++;
+			buf[i] = '\0';
+			ft_lstadd_back(list, ft_lstnew(ft_xstrdup(buf)));
+		}
 	}
+	free(buf);
 }
 
-static void	expand_var(t_list *separated)
+static t_state_type	free_set_return_state(t_list *separated, t_state_type state)
+{
+	free_set(&separated->content, ft_xstrdup(""));
+	return (state);
+}
+
+static t_list	*expand_env(t_list *separated)
 {
 	char	*content;
 	char	*tmp;
 
+	if (separated->next == NULL)
+		free_set(&separated->content, ft_xstrdup("$"));
+	else
+	{
+		free_set(&separated->content, ft_xstrdup(""));
+		separated = separated->next;
+		content = (char *)separated->content;
+		tmp = wrapper_hash_getstr(g_shell.env, content);
+		free_set(&separated->content, ft_xstrdup(tmp));
+	}
+	return (separated);
+}
+
+static void	expand_var(t_list *separated)
+{
+	t_state_type	state;
+	char			*content;
+
+	state = STATE_GENERAL;
 	while (separated)
 	{
 		content = (char *)separated->content;
-		if (content[0] == CHAR_DOL && content[1] != '\0')
-		{
-			tmp = wrapper_hash_getstr(g_shell.env, content + 1);
-			free_set(&separated->content, ft_xstrdup(tmp));
-		}
+		if (ft_strcmp(content, "\'") == 0 && state == STATE_GENERAL)
+			state = free_set_return_state(separated, STATE_IN_QUOTE);
+		else if (ft_strcmp(content, "\'") == 0 && state == STATE_IN_QUOTE)
+			state = free_set_return_state(separated, STATE_GENERAL);
+		else if (ft_strcmp(content, "\"") == 0 && state == STATE_GENERAL)
+			state = free_set_return_state(separated, STATE_IN_DQUOTE);
+		else if (ft_strcmp(content, "\"") == 0 && state == STATE_IN_DQUOTE)
+			state = free_set_return_state(separated, STATE_GENERAL);
+		else if (ft_strcmp(content, "$") == 0 && state != STATE_IN_QUOTE)
+			separated = expand_env(separated);
 		separated = separated->next;
 	}
 }
@@ -47,8 +82,6 @@ char	*minishell_expand(char *arg)
 
 	if (arg == NULL)
 		ft_fatal("minishell_expand : Invalid argument");
-	if (ft_strchr(arg, CHAR_DOL) == NULL)
-		return (ft_xstrdup(arg));
 	separated = NULL;
 	separate_to_list(arg, &separated);
 	expand_var(separated);
