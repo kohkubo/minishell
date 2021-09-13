d5a64a2e8ad376622dafdc82decb5ddd39186bfd
kohkubo <kohkubo@student.42tokyo.jp>
2021-09-13 11:54:42 +0900

    fix(expand.c/minishell_expand()): アルゴリズムを変更
    クォートの展開し、stateを持ってから、変数展開を行うように変更

--- srcs/expand/expand.c
+++ srcs/expand/expand.c
@@ -1,55 +1,33 @@
 #include "shell.h"
 
-static void	separate_to_list(char *arg, t_list **list)
+static char	*expand_env(char *arg)
 {
-	char	*buf;
-	int		i;
-
-	buf = ft_xmalloc(sizeof(char) * (ft_strlen(arg) + 1));
-	while (*arg != '\0')
-	{
-		if (*arg == CHAR_DOL || *arg == CHAR_QUOTE
-			|| *arg == CHAR_DQUOTE || *arg == CHAR_SPACE)
-		{
-			ft_lstadd_back(list, ft_lstnew(ft_xsubstr(arg, 0, 1)));
-			arg++;
-		}
-		else
-		{
-			i = 0;
-			while (*arg != '\0'
-				&& *arg != CHAR_DOL && *arg != CHAR_QUOTE
-				&& *arg != CHAR_DQUOTE && *arg != CHAR_SPACE)
-				buf[i++] = *arg++;
-			buf[i] = '\0';
-			ft_lstadd_back(list, ft_lstnew(ft_xstrdup(buf)));
-		}
-	}
-	free(buf);
-}
-
-static t_list	*expand_env(t_list *separated)
-{
-	char	*content;
+	t_list	*list;
+	t_list	*head;
 	char	*tmp;
 
-	if (separated->next == NULL
-		|| ft_isspace(((char *)separated->next->content)[0]))
-		free_set(&separated->content, ft_xstrdup("$"));
-	else
+	list = separate_to_list(arg, "$ \t\n\v\f\r");
+	head = list;
+	while (list)
 	{
-		free_set(&separated->content, NULL);
-		separated = separated->next;
-		content = (char *)separated->content;
-		tmp = (char *)hash_getstr(g_shell.env, content);
-		if (tmp == NULL)
-			tmp = "";
-		free_set(&separated->content, ft_xstrdup(tmp));
+		if (ft_strcmp((char *)list->content, "$") == 0 && list->next != NULL
+			&& ft_strchrset((char *)list->next->content, " \t\n\v\f\r") == NULL)
+		{
+			free_set(&list->content, NULL);
+			list = list->next;
+			tmp = (char *)hash_getstr(g_shell.env, (char *)list->content);
+			if (tmp == NULL)
+				tmp = "";
+			free_set(&list->content, ft_xstrdup(tmp));
+		}
+		list = list->next;
 	}
-	return (separated);
+	tmp = lst_to_string(head);
+	ft_lstclear(&head, free);
+	return (tmp);
 }
 
-static void	expand_var(t_list *separated)
+static void	expand_quote(t_list *separated)
 {
 	t_state_type	state;
 	char			*content;
@@ -66,8 +44,9 @@ static void	expand_var(t_list *separated)
 			free_set(&separated->content, NULL), state = STATE_IN_DQUOTE;
 		else if (ft_strcmp(content, "\"") == 0 && state == STATE_IN_DQUOTE)
 			free_set(&separated->content, NULL), state = STATE_GENERAL;
-		else if (ft_strcmp(content, "$") == 0 && state != STATE_IN_QUOTE)
-			separated = expand_env(separated);
+		else if (ft_strchr(content, CHAR_DOL) != NULL
+			&& state != STATE_IN_QUOTE)
+			free_set(&separated->content, expand_env(content));
 		separated = separated->next;
 	}
 }
@@ -79,9 +58,8 @@ char	*minishell_expand(char *arg)
 
 	if (arg == NULL)
 		ft_fatal("minishell_expand : Invalid argument");
-	separated = NULL;
-	separate_to_list(arg, &separated);
-	expand_var(separated);
+	separated = separate_to_list(arg, "\'\"\t\n\v\f\r");
+	expand_quote(separated);
 	ret = lst_to_string(separated);
 	ft_lstclear(&separated, free);
 	return (ret);
