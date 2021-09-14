87a57e71ac7ebd6120f42c3f3318b6d79940fe96
kohkubo <kohkubo@student.42tokyo.jp>
2021-09-14 19:55:34 +0900

    refactor(libex): レビュー内容を反映

--- libft/libex/ft_strchrset.c
+++ libft/libex/ft_strchrset.c
@@ -2,17 +2,10 @@
 
 char	*ft_strchrset(const char *s, char *charset)
 {
-	int	i;
-
 	while (*s)
 	{
-		i = 0;
-		while (charset[i])
-		{
-			if (*s == charset[i])
-				return ((char *)s);
-			i++;
-		}
+		if (ft_strchr(charset, *s))
+			return ((char *)s);
 		s++;
 	}
 	return (NULL);
---
+++ libft/libex/lst_join_str.c
@@ -0,0 +1,24 @@
+#include "libex.h"
+
+char	*lst_join_str(t_list *lst, char *sep)
+{
+	char	*str;
+
+	if (lst == NULL)
+		ft_fatal("lst_to_string: Invalid argument");
+	if (sep == NULL)
+		sep = "";
+	if ((char *)lst->content != NULL)
+		str = ft_xstrdup((char *)lst->content);
+	else
+		str = ft_xstrdup("");
+	while (lst->next)
+	{
+		free_set((void **)&str, ft_xstrjoin(str, sep));
+		if ((char *)lst->next->content != NULL)
+			free_set((void **)&str,
+				ft_xstrjoin(str, (char *)lst->next->content));
+		lst = lst->next;
+	}
+	return (str);
+}
