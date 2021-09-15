fc9c6652cb10ff537ee9965987b6f8e7f72506fc
kohkubo <kohkubo@student.42tokyo.jp>
2021-09-15 11:47:45 +0900

    test(env): add: testcase

--- tests/issue/env/test.sh
+++ tests/issue/env/test.sh
@@ -7,12 +7,14 @@ cd "$(dirname "$0")" || exit
 echo $PWD
 
 for file in $(ls *.txt); do
+	echo "File: $file"
 	./minishell < $file >> output 2>&1
 	bash < $file >> expect 2>&1
 	grep -v -e "SHLVL" -v -e "_=" expect | sort --output=expect
 	grep -v -e "SHLVL" -v -e "_=" output | sort --output=output
-	diff expect output
+	diff expect output > /dev/null 2>&1
 	if [ $? -ne 0 ]; then
+		diff expect output -y
 		echo "FAIL: $file"
 		RES=1
 	fi
