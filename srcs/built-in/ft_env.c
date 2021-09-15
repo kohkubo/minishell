4575e7b355ebc88f4b254f336cf58b885db8743f
kohkubo <kohkubo@student.42tokyo.jp>
2021-09-15 11:46:51 +0900

    feat(ft_env.c/ft_env()): 引数を無視するように変更

--- srcs/built-in/ft_env.c
+++ srcs/built-in/ft_env.c
@@ -12,18 +12,11 @@ static void	print_env_array(char **array)
 
 int	ft_env(char **args)
 {
-	char			**env;
-	char			**tmp;
-	t_hash_table	*env_hash;
+	char	**env;
 
-	tmp = hash_getall(g_shell.env, NULL);
-	env_hash = hash_create_table(1024);
-	store_shellenv(tmp, env_hash);
-	store_shellenv(args, env_hash);
-	env = hash_getall(env_hash, NULL);
+	(void)args;
+	env = hash_getall(g_shell.env, NULL);
 	print_env_array(env);
 	free_string_array(env);
-	free_string_array(tmp);
-	hash_clear_table(&env_hash);
 	return (0);
 }
