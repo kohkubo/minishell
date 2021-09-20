14f89db9861528a844ac4ee07719ff43630e665e
kohkubo <kohkubo@student.42tokyo.jp>
2021-09-20 14:44:30 +0900

    test(cd): cd testcase

---
+++ tests/issue/cd/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+cp ./minishell "$(dirname "$0")"
+
+cd "$(dirname "$0")" || exit
+
+echo $PWD
+
+rm -rf expect output leaksout
+
+for file in $(ls *.txt); do
+	mkdir -p ./testcd1/testcd2/testcd3
+	mkdir -p ./testcd4/testcd5/testcd6
+	echo "aaa" > ./testcd4/testcd5/testcd6/test00.txt
+	echo "bbb" > ./testcd4/testcd5/testcd6/test01.txt
+	echo "ccc" > ./testcd4/testcd5/testcd6/test02.txt
+	echo "Testing $file"
+	zsh < $file >> expect 2>&1
+	./minishell < $file >> output 2>&1
+	diff expect output &> /dev/null
+	if [ $? -ne 0 ]; then
+		echo "FAIL: $file"
+		cat $file
+		diff expect output -y -W 180
+		RES=1
+		exit 1
+	fi
+	cd /Users/kohkubo/GitHub/42_master/minishell/tests/issue/cd
+	rm -rf output expect testcd1 testcd4
+done
+
+rm -f a.out leaksout minishell
+rm -rf a.out.dSYM
+
+exit $RES
