4cadfcec02a8a6b46d168094efa4632e1af9442d
kohkubo <kohkubo@student.42tokyo.jp>
2021-09-30 21:01:02 +0900

    add testcase

---
+++ tests/issue/exec_pipe/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cp ./minishell "$(dirname "$0")"
+
+cd "$(dirname "$0")" || exit
+
+echo $PWD
+
+rm -rf output expect
+
+for file in $(ls *.txt); do
+	echo "--Testing $file"
+	./minishell < $file > output 2>&1
+	bash < $file > expect 2>&1
+	diff -y expect output # &> /dev/null
+	if [ $? -ne 0 ]; then
+		echo "FAIL: $file"
+		cat $file
+		diff -y expect output
+		RES=1
+		exit
+	fi
+	rm -rf output expect
+done
+
+rm -f a.out leaksout minishell
+rm -rf a.out.dSYM
+
+exit $RES
