8e5418a0448def3bc8f4aac7bb3b978e1a66c229
kohkubo <kohkubo@student.42tokyo.jp>
2021-10-05 18:57:02 +0900

    echo exit status testcase

---
+++ tests/issue/echo_exit_status/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cp ./minishell "$(dirname "$0")"
+
+cd "$(dirname "$0")" || exit
+
+echo $PWD
+
+rm -rf expect output
+
+for file in $(ls *.txt); do
+	echo "Testing $file"
+	./minishell < $file >> output 2>&1
+	bash < $file >> expect 2>&1
+	diff -y expect output
+	if [ $? -ne 0 ]; then
+		echo "FAIL: $file"
+		cat $file
+		diff expect output -y
+		RES=1
+	fi
+	rm -rf output expect
+done
+
+rm -f a.out leaksout minishell
+rm -rf a.out.dSYM
+
+exit $RES
