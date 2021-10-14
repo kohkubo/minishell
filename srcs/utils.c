e58cf8ad38079eec068168d3ff9e85dc50c96cea
ywake <ywake@student.42tokyo.jp>
2021-10-14 23:55:48 +0900

    fix: 空のディレクトリ名はカレントディレクトリを示す

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,22 +1,57 @@
 #include "shell.h"
 
+#include "libft.h"
+#include "libex.h"
+
+char	*get_data(const char *path, int start, int end)
+{
+	if (end - start == 0)
+		return (ft_xstrdup("."));
+	return (ft_xsubstr(path, start, end - start));
+}
+
+t_list	*split_path(const char *path)
+{
+	t_list	*list;
+	size_t	i;
+	size_t	start;
+
+	list = NULL;
+	i = 0;
+	start = 0;
+	while (path[i])
+	{
+		if (path[i] == ':')
+		{
+			ft_lstadd_back(&list, ft_xlstnew(get_data(path, start, i)));
+			start = i + 1;
+		}
+		i++;
+	}
+	ft_lstadd_back(&list, ft_xlstnew(get_data(path, start, i)));
+	return (list);
+}
+
 char	**get_fullpath(const char *path, char *cmd)
 {
-	char	**paths;
-	int		i;
+	t_list	*paths;
+	t_list	*save;
+	char	*data;
+	char	**ret;
 
 	if (path == NULL || cmd == NULL)
 		return (NULL);
-	paths = catch_null(ft_split(path, ':'), "split");
-	i = 0;
-	while (paths[i])
+	paths = split_path(path);
+	save = paths;
+	while (paths)
 	{
-		if (paths[i][ft_strlen(paths[i]) - 1] != '/')
-			free_set((void **)&paths[i],
-				catch_null(ft_strjoin(paths[i], "/"), "ft_strjoin"));
-		free_set((void **)&paths[i],
-			catch_null(ft_strjoin(paths[i], cmd), "ft_strjoin"));
-		i++;
+		data = paths->content;
+		if (data[ft_strlen(data) - 1] != '/')
+			free_set((void **)&data, ft_xstrjoin(data, "/"));
+		free_set((void **)&data, ft_xstrjoin(data, cmd));
+		paths = paths->next;
 	}
-	return (paths);
+	ret = lst_to_array(save);
+	ft_lstclear(&save, free);
+	return (ret);
 }
