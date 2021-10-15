ec75019fe7ffa84e477cb0ba05198e5abf96bc33
kohkubo <kohkubo@student.42tokyo.jp>
2021-10-15 17:39:21 +0900

    testcase update

--- tests/issue/expand/test.sh
+++ tests/issue/expand/test.sh
@@ -9,8 +9,10 @@ echo $PWD
 rm -rf expect output
 
 for file in $(ls *.txt); do
-	echo "Testing $file"
+	# echo "Testing $file"
+	# ./minishell < $file | sed '/minishell> /d' >> output 2>&1
 	./minishell < $file >> output 2>&1
+	sed -i "" -e "/minishell> /d" output
 	zsh < $file >> expect 2>&1
 	diff expect output &> /dev/null
 	if [ $? -ne 0 ]; then
