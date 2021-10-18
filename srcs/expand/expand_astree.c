251daa210d6dc4b1d91322756c651ededbdbdaf1
ywake <ywake@student.42tokyo.jp>
2021-10-18 16:34:18 +0900

    fix: expand_astreeでheredocとその他で展開方法を分ける

--- srcs/expand/expand_astree.c
+++ srcs/expand/expand_astree.c
@@ -7,7 +7,14 @@ void	expand_astree(t_astree *tree)
 	if (tree == NULL)
 		return ;
 	if (tree->type & NODE_DATA)
-		free_set((void **)&tree->data, minishell_expand(tree->data));
+	{
+		if (tree->type & NODE_REDIRECT_IN2)
+			free_set((void **)&tree->data,
+				minishell_expand(tree->data, expand_handler_heredoc));
+		else
+			free_set((void **)&tree->data,
+				minishell_expand(tree->data, expand_handler));
+	}
 	expand_astree(tree->right);
 	expand_astree(tree->left);
 }
