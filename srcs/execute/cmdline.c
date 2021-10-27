555122914de24827da0869ba2a6013b2ebb80f43
ywake <ywake@student.42tokyo.jp>
2021-10-28 08:02:23 +0900

    Support: [Exec] `&&`, `||`

--- srcs/execute/cmdline.c
+++ srcs/execute/cmdline.c
@@ -1,5 +1,15 @@
 #include "exec.h"
 
+#include "astree.h"
+
+enum e_mean
+{
+	_,
+	LAST,
+	STDIN_BACKUP,
+	LEN
+};
+
 /*
 ** <command line>	::= <job> ';' <command line>	// ignore
 **					  | <job> ';'					// ignore
@@ -9,5 +19,18 @@
 */
 void	execute_cmdline(t_astree *tree, int *status)
 {
-	execute_job(tree, status);
+	if (!(tree->type & NODE_AND) && !(tree->type & NODE_OR))
+		return ((void)execute_job(tree, status));
+	if (tree->type & NODE_AND)
+	{
+		execute_job(tree->left, status);
+		if (get_status(*status) == 0)
+			execute_job(tree->right, status);
+	}
+	else if (tree->type & NODE_OR)
+	{
+		execute_job(tree->left, status);
+		if (get_status(*status) != 0)
+			execute_job(tree->right, status);
+	}
 }
